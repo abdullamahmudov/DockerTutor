9694d917793cfc2636130dadb80fe30812c2a8cf
Mahmudov Abdulla <abdulla.mahmudov0592@gmail.com>
2026-01-14 19:06:18 +0300

    Merge remote-tracking branch 'origin/HEAD'

---
+++ HM/ParallelText/TextParser/TextCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using TextParser.Interfaces;
+
+namespace TextParser;
+
+public class TextCalculator : ITextCalculator
+{
+    /// <inheritdoc/>
+    public Task CalculateSpaces(string text)
+    {
+        var stopwatch = new Stopwatch();
+        var count = 0;
+        stopwatch.Start();
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] != ' ')
+                continue;
+
+            count++;
+        }
+        stopwatch.Stop();
+        Console.WriteLine(string.Concat("КОЛЛИЧЕСТВО ПРОБЕЛОВ В ТЕКСТЕ: ", count,"\nВРЕМЯ ПОДСЧЕТА ПРОБЕЛОВ: ", stopwatch.ElapsedMilliseconds, " milliseconds","\n- - - - - - - - - - - - -"));
+        return Task.CompletedTask;
+    }
+}
